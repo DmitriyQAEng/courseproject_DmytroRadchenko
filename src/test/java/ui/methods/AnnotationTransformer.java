6e6a217be934787941785b51f645f5d831f3594a
Dmitriy Radchenko <djdmonqa@gmail.com>
2024-02-03 23:43:10 +0200

    fixed retry analyzer

--- src/test/java/ui/methods/AnnotationTransformer.java
+++ src/test/java/ui/methods/AnnotationTransformer.java
@@ -1,2 +1,18 @@
-package ui.methods;public class AnnotationTransformer {
+package ui.methods;
+
+import org.testng.IAnnotationTransformer;
+import org.testng.annotations.ITestAnnotation;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+public class AnnotationTransformer implements IAnnotationTransformer {
+
+    @Override
+    public void transform(ITestAnnotation annotation,
+                          Class testClass,
+                          Constructor testConstructor,
+                          Method testMethod) {
+        annotation.setRetryAnalyzer(RetryAnalyzer.class);
+    }
 }
