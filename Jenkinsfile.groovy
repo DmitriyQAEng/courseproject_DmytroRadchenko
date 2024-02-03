7d6d23dc22cab97eb8e553a4c658097bb13a851e
Dmitriy Radchenko <djdmonqa@gmail.com>
2024-02-04 00:25:31 +0200

    edited jenkinfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,10 +4,10 @@ pipeline {
         timeout(time: 1, unit: 'HOURS')
     }
     triggers {
-        cron('0 9 * * *')
+        cron('0 18 * * *')
     }
     parameters {
-         string(name: 'MyRegressionSuite', description: 'Enter the name of the Test Suite', defaultValue: 'MyTestSuite')
+         string(name: 'suite', description: 'Enter the name of the Test Suite', defaultValue: 'FullTest')
          string(name: 'baseUrl', description: 'Validate Application URL', defaultValue: 'http://localhost')
     }
     environment {
@@ -18,15 +18,10 @@ pipeline {
         maven 'Maven'
     }
     stages {
-        stage("Build the Project") {
-            steps {
-                sh "mvn clean install -DskipTests"
-            }
-        }
         stage("Run Tests") {
             steps {
                 script {
-                    sh "mvn clean test -Dsurefire.suiteXmlFiles=src/test/resources/ApiTest.xml"
+                    sh "./mvnw test"
                 }
             }
         }
