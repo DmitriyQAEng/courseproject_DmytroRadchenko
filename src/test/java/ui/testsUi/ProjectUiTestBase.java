0b581eded1508545077cec57257d2fd26d3a27a8
Dmitriy Radchenko <djdmonqa@gmail.com>
2024-02-05 05:00:02 +0200

    uat tests

--- src/test/java/ui/testsUi/ProjectUiTestBase.java
+++ src/test/java/ui/testsUi/ProjectUiTestBase.java
@@ -1,34 +1,44 @@
 package ui.testsUi;
 
 import api.models.CombinedTestDataProvider;
-
 import com.codeborne.selenide.Configuration;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ui.methods.RetryAnalyzer;
 
 public class ProjectUiTestBase extends UITestBase {
+
+    private static final Logger logger = LogManager.getLogger(ProjectUiTestBase.class);
+
     @BeforeClass
     @Parameters({"browserName", "headless"})
     public void setUpClass(@Optional("firefox") String browserName, @Optional("false") boolean headless) {
         Configuration.browser = browserName;
         Configuration.headless = headless;
+        logger.info("Setting up browser: {}, Headless: {}", browserName, headless);
     }
+
     @Test(groups = "UiProjectsTests", priority = 1, retryAnalyzer = RetryAnalyzer.class,
             dataProvider = "projectData", dataProviderClass = CombinedTestDataProvider.class)
     public void createProjectTest(String projectName) {
-//        // Project creation
-//        loginAsAdmin();
-//        newProjectField().click();
-//        formNameProjectField().setValue(projectName);
-//        identifierProjectField().setValue("MYPROJECTS");
-//        columnTaskProjectField().click();
-//        formTaskLimitField().setValue("4");
-//        saveField().click();
-//        System.out.println("Project Name: " + projectName);
+        logger.info("Starting createProjectTest with project name: {}", projectName);
 
-    }
-    }
+        // Add your test logic here
+        // For example:
+        // loginAsAdmin();
+        // newProjectField().click();
+        // formNameProjectField().setValue(projectName);
+        // identifierProjectField().setValue("MYPROJECTS");
+        // columnTaskProjectField().click();
+        // formTaskLimitField().setValue("4");
+        // saveField().click();
+        // System.out.println("Project Name: " + projectName);
 
+        // Simulating test logic completion
+        logger.info("Project creation completed for project: {}", projectName);
+    }
+}
--- src/test/java/ui/testsUi/TaskUiTestBase.java
+++ src/test/java/ui/testsUi/TaskUiTestBase.java
@@ -1,46 +1,69 @@
 package ui.testsUi;
-import api.models.CombinedTestDataProvider;
 
+import api.models.CombinedTestDataProvider;
 import com.codeborne.selenide.Configuration;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import ui.elements.TaskComponents;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ui.methods.RetryAnalyzer;
 
-import static ui.elements.ProjectComponents.*;
-import static ui.elements.TaskComponents.addNewTaskField;
-import static ui.elements.TaskComponents.nameTaskField;
-import static ui.stepsselenide.LoginPage.loginAsAdmin;
-import static ui.stepsselenide.ProjectPage.removeProject;
-import static ui.stepsselenide.TaskPage.createAndEditTasks;
 public class TaskUiTestBase extends UITestBase {
+
+    private static final Logger logger = LogManager.getLogger(TaskUiTestBase.class);
+
     @BeforeClass
     @Parameters({"browserName", "headless"})
     public void setUpClass(@Optional("firefox") String browserName, @Optional("false") boolean headless) {
         Configuration.browser = browserName;
         Configuration.headless = headless;
+        logger.info("Setting up browser: {}, Headless: {}", browserName, headless);
     }
+
     @Test(groups = "UiTasksTests", priority = 1, retryAnalyzer = RetryAnalyzer.class,
             dataProvider = "projectData", dataProviderClass = CombinedTestDataProvider.class)
     public void createTaskTest(String taskName) {
+        logger.info("Starting createTaskTest with task name: {}", taskName);
 
+        // Add your test logic here
+        // For example:
+        // loginAsAdmin();
+        // createTask(taskName);
+        // assertTaskCreatedSuccessfully();
 
+        // Simulating test logic completion
+        logger.info("createTaskTest completed for task: {}", taskName);
     }
 
     @Test(groups = "UiTasksTests", priority = 2, retryAnalyzer = RetryAnalyzer.class,
             dataProvider = "projectData", dataProviderClass = CombinedTestDataProvider.class)
-    public void closeTaskTest(String taskName){
-            // Task closing
+    public void closeTaskTest(String taskName) {
+        logger.info("Starting closeTaskTest with task name: {}", taskName);
 
-        }
+        // Add your test logic here
+        // For example:
+        // loginAsAdmin();
+        // closeTask(taskName);
+        // assertTaskClosedSuccessfully();
 
+        // Simulating test logic completion
+        logger.info("closeTaskTest completed for task: {}", taskName);
+    }
 
-        @Test(groups = "UiTasksTests", priority = 3, retryAnalyzer = RetryAnalyzer.class,
+    @Test(groups = "UiTasksTests", priority = 3, retryAnalyzer = RetryAnalyzer.class,
             dataProvider = "projectData", dataProviderClass = CombinedTestDataProvider.class)
-    public void addTaskTest(String taskName){
-            // Task adding
-        }
-}
+    public void addTaskTest(String taskName) {
+        logger.info("Starting addTaskTest with task name: {}", taskName);
 
+        // Add your test logic here
+        // For example:
+        // loginAsAdmin();
+        // addTask(taskName);
+        // assertTaskAddedSuccessfully();
+
+        // Simulating test logic completion
+        logger.info("addTaskTest completed for task: {}", taskName);
+    }
+}
