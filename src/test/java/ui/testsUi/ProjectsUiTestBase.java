6d3512d91c626ea50d0d6ba8b8755d86c885586c
Dmitriy Radchenko <djdmonqa@gmail.com>
2024-02-05 03:35:22 +0200

    uat tests

--- src/test/java/ui/testsUi/ProjectsUiTestBase.java
+++ src/test/java/ui/testsUi/ProjectsUiTestBase.java
@@ -12,29 +12,23 @@ import static ui.stepsselenide.TaskPage.createAndEditTasks;
 
 public class ProjectsUiTestBase extends UITestBase {
 
-    @Test(groups = "UiProjectsTests", priority = 1, retryAnalyzer = RetryAnalyzer.class,
-            dataProvider = "projectData", dataProviderClass = CombinedTestDataProvider.class)
-    public void createProjectTest(String projectName) {
-        // Project creation
-        loginAsAdmin();
-        newProjectField().click();
-        formNameProjectField().setValue(projectName);
-        identifierProjectField().setValue("MYPROJECTS");
-        columnTaskProjectField().click();
-        formTaskLimitField().setValue("4");
-        saveField().click();
-        System.out.println("Project Name: " + projectName);
+//    @Test(groups = "UiProjectsTests", priority = 1, retryAnalyzer = RetryAnalyzer.class,
+//            dataProvider = "projectData", dataProviderClass = CombinedTestDataProvider.class)
+//    public void createProjectTest(String projectName) {
+//        // Project creation
+//        loginAsAdmin();
+//        newProjectField().click();
+//        formNameProjectField().setValue(projectName);
+//        identifierProjectField().setValue("MYPROJECTS");
+//        columnTaskProjectField().click();
+//        formTaskLimitField().setValue("4");
+//        saveField().click();
+//        System.out.println("Project Name: " + projectName);
+
+
+
+
+
+
     }
 
-    @Test(groups = "UiProjectsTests", priority = 2, retryAnalyzer = RetryAnalyzer.class)
-    public void createTaskTest() {
-        // Create/edit/remove the Task
-        createAndEditTasks();
-    }
-
-    @Test(groups = "UiProjectsTests", priority = 3, retryAnalyzer = RetryAnalyzer.class)
-    public void removeProjectsTest() {
-        // Project remove
-        removeProject();
-    }
-}
---
+++ src/test/java/ui/testsUi/TaskUiTestBase.java
@@ -0,0 +1,34 @@
+package ui.testsUi;
+import api.models.CombinedTestDataProvider;
+
+import org.testng.annotations.Test;
+import ui.methods.RetryAnalyzer;
+
+import static ui.elements.ProjectComponents.*;
+import static ui.stepsselenide.LoginPage.loginAsAdmin;
+import static ui.stepsselenide.ProjectPage.removeProject;
+import static ui.stepsselenide.TaskPage.createAndEditTasks;
+public class TaskUiTestBase extends UITestBase {
+
+//    @Test(groups = "UiTasksTests", priority = 1, retryAnalyzer = RetryAnalyzer.class,
+//            dataProvider = "projectData", dataProviderClass = CombinedTestDataProvider.class)
+//    public void createTaskTest(String taskName) {
+//        // Task creation
+
+
+
+//    @Test(groups = "UiTasksTests", priority = 2, retryAnalyzer = RetryAnalyzer.class,
+//            dataProvider = "projectData", dataProviderClass = CombinedTestDataProvider.class)
+//    public void closeTaskTest(String taskName) {
+//        // Task closing
+
+
+
+
+    //    @Test(groups = "UiTasksTests", priority = 3, retryAnalyzer = RetryAnalyzer.class,
+//            dataProvider = "projectData", dataProviderClass = CombinedTestDataProvider.class)
+//    public void addTaskTest(String taskName) {
+//        // Task adding
+
+}
+
